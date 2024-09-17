f84d4825f35b05869a9bdbcf21efedd773c65f95
kysx15 <ksxyeo@gmail.com>
2024-09-17 23:51:04 +0800

    Added functions and necessary code for records

--- disk/storage.h
+++ disk/storage.h
@@ -1,6 +1,7 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
+#include "../struct.h"
 #include <cstddef> 
 
 class Disk {
@@ -10,16 +11,24 @@ public:
 
     bool allocateBlock();
     void initializeDisk();
+    bool storeRecord(Record record);
 
     size_t getTotalSize() const;
     size_t getBlockSize() const;
     size_t getNumBlocks() const;
+    size_t getRecordSize() const;
+    size_t getNumRecords() const;
+    size_t getNumRecordsInBlock() const;
 
 private:
     unsigned char* startAddress;
     size_t totalSize;
     size_t blockSize;
-    size_t usedBlocks;
+    size_t usedBlocks; // number of blocks used
+    size_t usedBlockMemory; // amount of memory used in current block
+    size_t recordSize; 
+    size_t numRecords; // number of records
+    size_t numRecordsInBlock; // number of records in current block
 };
 
 #endif
